a97a2a406b5be228232dcf9afd0eabc55725b278
Lukasz Lach <lukasz.lach@getresponse.com>
2020-10-20 22:58:46 +0200

    Change text of page heading

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export class App extends React.PureComponent {
     public render(): React.ReactNode {
         return (
             <AppStyledMainWrapper>
-                <h1>Recruitment App</h1>
+                <h1>GitHub Users App</h1>
                 <Switch>
                     <Route
                         exact
